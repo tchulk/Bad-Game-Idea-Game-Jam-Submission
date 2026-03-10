4786a188c0fb02e658b5db5f0020887cd2558e93
tchulk <99852935+tchulk@users.noreply.github.com>
2026-03-10 15:08:23 +1100

    Added an Interaction manager
    I Added an Interaction manager to the game to help streamline the making of the minigames.

---
+++ NPCHero2D/Assets/Script/Combat/Player/PlayerInteractionManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerInteractionManager : MonoBehaviour
+{
+    private PlayerInputAction playerInput;
+    [SerializeField] private LayerMask layerMask;
+
+    private PlayerMovement playerMovement;
+    private Vector3 directionofRay;
+
+    private void Awake()
+        {
+            playerInput = new PlayerInputAction();
+            playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Player.Enable();
+        playerInput.Player.Interact.performed += ctx => Interact();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Player.Interact.performed -= ctx => Interact();
+        playerInput.Player.Disable();
+    }
+
+    public void Interact()
+    {
+        if (playerMovement.isFacingRight)
+        {
+            directionofRay = transform.right;
+            //return;
+        }
+        if (playerMovement.isFacingLeft)
+        {
+            directionofRay = -transform.right;
+            //return;
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionofRay, 2f, layerMask);
+        Debug.DrawRay(transform.position, directionofRay * 2f, Color.blue, 1);
+        if (hit.collider != null)
+        {
+            Debug.Log("Interacted with " + hit.collider.name);
+            // Implement interaction logic here (e.g., open a door, pick up an item, etc.)
+        }
+    }
+}
