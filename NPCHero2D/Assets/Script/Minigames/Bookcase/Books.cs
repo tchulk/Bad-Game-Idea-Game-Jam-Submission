241f0ba5bcf2c01e0bd153fa8235040e1e2bbf71
tchulk <99852935+tchulk@users.noreply.github.com>
2026-03-30 07:40:31 +1100

    Made it testing ready

--- NPCHero2D/Assets/Script/Minigames/Bookcase/Books.cs
+++ NPCHero2D/Assets/Script/Minigames/Bookcase/Books.cs
@@ -6,12 +6,18 @@ public class Folder : MonoBehaviour
     [SerializeField] private GameObject Bookcase;
 
     private bool DoOnce = false;
+    private bool isInPosition = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
 
     // Update is called once per frame
     void Update()
     {
+
+        if (isInPosition == true)
+        {
+            transform.position = WhereBookShouldBe.position;
+        }
         if(DoOnce == false)
           {
             if (transform.position.x >= WhereBookShouldBe.position.x - 10 && transform.position.x <= WhereBookShouldBe.position.x + 10)
@@ -20,6 +26,7 @@ public class Folder : MonoBehaviour
                 {
                     Bookcase.GetComponent<Bookcase>().NumberOfBooks++;
                     DoOnce = true;
+                    isInPosition = true;
                     Debug.Log("Book added");
                 }
             }
